f7375e06c4b9830a1d328b49cad837a95e07536a
Kyle Robison <kylerobison15@gmail.com>
2021-06-25 15:02:30 -0600

    User Story 1 Complete

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -7,30 +7,68 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-  
-  DatabaseAccessor db = new DatabaseAccessorObject();
 
-  public static void main(String[] args) {
-    FilmQueryApp app = new FilmQueryApp();
-    app.test();
-//    app.launch();
-  }
+	DatabaseAccessor db = new DatabaseAccessorObject();
 
-  private void test() {
-    Film film = db.findFilmById(1);
-    System.out.println(film);
-  }
+	public static void main(String[] args) {
+		FilmQueryApp app = new FilmQueryApp();
+//		app.test();
+		app.launch();
+	}
 
-  private void launch() {
-    Scanner input = new Scanner(System.in);
-    
-    startUserInterface(input);
-    
-    input.close();
-  }
+//	private void test() {
+//		Film film = db.findFilmById(1);
+//		System.out.println(film);
+//	}
 
-  private void startUserInterface(Scanner input) {
-    
-  }
+	private void launch() {
+		Scanner input = new Scanner(System.in);
+
+		startUserInterface(input);
+
+		input.close();
+	}
+
+	private void startUserInterface(Scanner input) {
+		int userChoice = 0;
+		boolean usingMenu = true;
+
+		while (usingMenu) {
+			System.out.println("------- WELCOME TO THE FILM QUERY APP -------");
+			System.out.println("Please select an option from the menu below.");
+			System.out.println("---------------------------------------------");
+			System.out.println("1. Look up a film by its ID");
+			System.out.println("2. Look up a film by a keyword search");
+			System.out.println("3. Exit Application");
+			System.out.println("---------------------------------------------");
+
+			userChoice = input.nextInt();
+			input.nextLine();
+			usingMenu = doUserChoice(userChoice);
+		}
+		
+		
+	}
+
+	private boolean doUserChoice(int userChoice) {
+
+		switch (userChoice) {
+
+		case 1:
+			// TODO Add method call to look up film by it's ID
+			return true;
+		case 2:
+			// TODO Add method call to look up film by keyword search
+			return true;
+		case 3:
+			System.out.println("Thanks for using the Film Query App - Goodbye!");
+			return false;
+		default:
+			System.out.println("Your entry was invalid. Please enter the number of the cooresponding menu item.");
+			return true;
+
+		}
+
+	}
 
 }
--- src/com/skilldistillery/filmquery/entities/Film.java
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -141,7 +141,9 @@ public class Film {
 		this.actors = actors;
 	}
 
-
+	public void displayFilmInfo() {
+		
+	}
 
 	@Override
 	public String toString() {
